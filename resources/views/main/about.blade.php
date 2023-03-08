c178a5d17a1a41798275bcae45bbe5904db3ba8d
Jollyver717 <2001103921@student.buksu.edu.ph>
2023-03-08 16:57:05 +0800

    crud

---
+++ resources/views/main/about.blade.php
@@ -0,0 +1,29 @@
+@extends("layouts.master")
+
+@section("title")
+About us 
+@endsection
+
+@section("content")
+        <section class="page-section about-heading">
+            <div class="container">
+                <img src="https://i.ytimg.com/vi/hgdlXhASevw/maxresdefault.jpg" alt="">
+                <div class="about-heading-content">
+                    <div class="row">
+                        <div class="col-xl-9 col-lg-10 mx-auto">
+                            <div class="bg-faded rounded p-5">
+                                <h2 class="section-heading mb-4">
+                                    <span class="section-heading-upper">The Best and Qaulity Street Foods in Town!</span>
+                                    <span class="section-heading-lower">About Our Street Foods</span>
+                                </h2>
+                                <p>Our Street Foods business started on 2022, it all came from the idea of my brother who is a graduate on the course financial management, his idea came up to an action that's why we decided we put it into business and make a small store. </p>
+                                <p class="mb-0">
+                                    We guarantee a good quality street foods products. We ensure you a fresh and quality street foods you will ever had, our stocks provider is the most trusted street foods dealer in the Country. We also assure to serve you the best we can do.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+ @endsection       
